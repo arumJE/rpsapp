e87b1aeca8135b13b277ce4dce78bbcfcc949dc6
Arum Galadima <arum.galadima@jacobseye.com>
2019-02-26 15:07:25 -0500

    JE email validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -108,9 +108,26 @@ class Login extends Component {
       });
     }
 
+    validateEmail(email) {
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(re.test(email)){
+        //Email valid. Procees to test if it's from the right domain (Second argument is to check that the string ENDS with this domain, and that it doesn't just contain it)
+        if(email.indexOf("@jacobseye.com", email.length - "@jacobseye.com".length) !== -1){
+          //VALID
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+
     handleSubmit(e) {
       e.preventDefault();
-      this.signin(e);
+      if(this.validateEmail(this.state.email)) {
+        this.signin(e);
+      } else {
+        alert('You may only signin with a Jacobs Eye domain');
+      }
       // this.databaseRecord();
       console.log('The form was submitted with the following data:');
       // console.log(this.state);
